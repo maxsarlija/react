6c8360345b933ae0f6d028ff71f35fb1f7fe1d70
max.sarlija <max.sarlija@outlook.com.ar>
2022-05-02 17:59:48 -0300

    desafio clase 3

--- clase2_proyecto/src/App.jsx
+++ clase2_proyecto/src/App.jsx
@@ -2,49 +2,25 @@ import './App.css'
 import './styles/Header.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Header from './components/header'
+import Formulario from './components/Formulario';
+import Header from './components/Header'
+import ItemListContainer from './components/ItemListContainer';
+import Titulo from './components/Titulo';
 import logo from './logo.svg'
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Titulo />
+      <Titulo titulo='Tiendita'/>
+      <ItemListContainer greeting='ItemListContainer PlaceHolder' />
+      <Formulario />    
+      
     </div>
   )
-}
+} 
+
 
 export default App
